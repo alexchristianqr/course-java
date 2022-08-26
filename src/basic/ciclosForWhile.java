20c952e125ca7717ca206275cef6b2cb02ac20f5
Alex Christian <alexchristianqr@gmail.com>
2022-08-26 15:21:22 -0500

    [updated] arreglos 2D y 3D

--- src/basic/ciclosForWhile.java
+++ src/basic/ciclosForWhile.java
@@ -2,29 +2,95 @@ package basic;
 
 public class ciclosForWhile {
     public static void main(String[] args) {
-        int counter = 10;
+        cicloFor();
+        cicloWhile();
+        cicloDoWhile();
+        arregloBidimensional();
+        arregloTridimensional();
+    }
+
+    static void cicloFor() {
+        int contador = 10;
 
         // Ciclo for
-        for (int index = 1; index <= counter; index++) {
-            System.out.println("El indice es " + index);
+        for (int indice = 1; indice <= contador; indice++) {
+            System.out.println("El indice es " + indice);
         }
-
         System.out.println("--");
+    }
+
+    static void cicloWhile() {
+        int contador = 10;
+        int indice = 1;
 
         // Ciclo while
-        int indice = 1;
-        while (indice <= counter) {
+        while (indice <= contador) {
             System.out.println("El indice es " + indice);
             indice++;
         }
-
         System.out.println("--");
+    }
+
+    static void cicloDoWhile() {
+        int contador = 10;
+        int indice = 0;
 
         // Ciclo do while
-        int indice2 = 0;
         do {
-            indice2++;
-            System.out.println("El indice es " + indice2);
-        } while (indice2 < counter);
+            indice++;
+            System.out.println("El indice es " + indice);
+        } while (indice < contador);
+        System.out.println("--");
+    }
+
+    static void arregloBidimensional() {
+        int filas = 3;// j
+        int columnas = 3;// i
+        int contador = 1;
+
+        // Arreglo bidimensional (2 dimensiones)
+        int[][] numeros = new int[filas][columnas];
+
+        // Iterar fila x fila
+        for (int j = 0; j < filas; j++) {
+
+            // Iterar columna x columna
+            for (int i = 0; i < columnas; i++) {
+                numeros[j][i] = contador;
+                contador++;
+                System.out.print("[" + numeros[j][i] + "]");
+            }
+            System.out.println("");
+        }
+        System.out.println("--");
+    }
+
+    static void arregloTridimensional() {
+        int caras = 3;// Z
+        int filas = 3;// Y
+        int columnas = 3;// X
+        int contador = 1;
+
+        // Arreglo tridimensional (3 dimensiones)
+        int[][][] numeros = new int[caras][filas][columnas];
+
+        // Iterar cara x cara
+        for (int z = 0; z < caras; z++) {
+            System.out.println("Cara " + z);
+
+            // Iterar fila x fila
+            for (int y = 0; y < filas; y++) {
+
+                // Iterar columna x columna
+                for (int x = 0; x < columnas; x++) {
+                    numeros[z][y][x] = contador;
+                    contador++;
+                    System.out.print("[" + numeros[z][y][x] + "]");
+                }
+                System.out.println("");
+            }
+            contador = 1;// Reiniciar contador de arreglo bidimensional
+        }
+        System.out.println("--");
     }
 }
