c51a7e85fcdde2ed55819c49dc5bb8e835d78143
Alex Christian <alexchristianqr@gmail.com>
2022-08-27 15:03:29 -0500

    [updated]

--- src/intermedio/Ejemplo_de_instancia.java
+++ src/intermedio/Ejemplo_de_instancia.java
@@ -4,7 +4,6 @@ public class Ejemplo_de_instancia {
     public static void main(String[] args) {
         // Instanciar la clase Profesor y utilizar sus metodos publicos
         Profesor profesor = new Profesor();
-        System.out.println(profesor.quienSoy());
         System.out.println(profesor.explicarTrabajo());
         System.out.println(profesor.desarrollarTrabajo());
     }
