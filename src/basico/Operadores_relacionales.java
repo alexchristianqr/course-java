0e6c5616f860a5c5304d184a4b09f710e00dab7d
Alex Christian <alexchristianqr@gmail.com>
2022-08-27 00:02:49 -0500

    [updated] clase de operadores combinadas

---
+++ src/basico/Operadores_relacionales.java
@@ -0,0 +1,32 @@
+package basico;
+
+public class Operadores_relacionales {
+    public static void main(String[] args) {
+        mayorMenor();
+        mayorIgual();
+    }
+
+    static void mayorMenor() {
+        int valor1 = 10;
+        int valor2 = 5;
+        boolean resultadoA = valor1 < valor2;
+        System.out.println("10 < 5 " + resultadoA);
+        boolean resultadoB = valor1 > valor2;
+        System.out.println("10 > 5 " + resultadoB);
+        boolean resultadoC = valor1 == valor2;
+        System.out.println("10 == 5 " + resultadoC);
+        System.out.println("--");
+    }
+
+    static void mayorIgual() {
+        int valor1 = 10;
+        int valor2 = 5;
+        boolean resultadoA = valor1 >= valor2;
+        System.out.println("10 >= 5 " + resultadoA);
+        boolean resultadoB = valor1 <= valor2;
+        System.out.println("10 <= 5 " + resultadoB);
+        boolean resultadoC = valor1 != valor2;
+        System.out.println("10 != 5 " + resultadoC);
+        System.out.println("--");
+    }
+}
