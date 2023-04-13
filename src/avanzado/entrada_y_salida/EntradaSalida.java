17e8292c01b474f02af6b776217a264b5794d615
Alex Christian <alexchristianqr@gmail.com>
2023-04-13 09:24:06 -0500

    [updated] leer archivo

--- src/avanzado/entrada_y_salida/EntradaSalida.java
+++ src/avanzado/entrada_y_salida/EntradaSalida.java
@@ -56,3 +56,16 @@ class EjemploCopiarArchivo {
         }
     }
 }
+
+class EjemploLeerArchivoTexto {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("archivo.txt"))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
